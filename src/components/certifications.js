59667e05d40c47b8490c5f30a9244d4bd2f4da9b
Mackenzie May <panicrazia@gmail.com>
2023-04-11 20:27:47 -0400

    made certificates less ugly

--- src/components/certifications.js
+++ src/components/certifications.js
@@ -15,21 +15,19 @@ const Certifications = () => {
     <div id="certifications">
         <h3>Here are some certifications I have, and ones I am gunning for in the future</h3>
         <div className="noMarkers " style={gridStyles}>
-            <div>
-                <a href="https://www.freecodecamp.org/certification/Mackenzie-May/responsive-web-design">
+            <a class="certificate" href="https://www.freecodecamp.org/certification/Mackenzie-May/responsive-web-design">
+                <p class="certificateHeader">Responsive Web Design</p>
                 <img src={responsiveWebDesignCertificate} alt="responsiveCert" width="100%" display="block"/>
-                </a>
-            </div>
-            <div>
-                <a href="https://www.freecodecamp.org/certification/Mackenzie-May/javascript-algorithms-and-data-structures">
+                <div class="certificateBackground"></div>
+            </a>
+            <a class="certificate" href="https://www.freecodecamp.org/certification/Mackenzie-May/javascript-algorithms-and-data-structures">
+                <p class="certificateHeader">Javascript and data structures</p>
                 <img src={javascriptCertificate} alt="javascriptCert" width="100%" display="block"/>
-                </a>
-            </div>
-            <div>
-                <a href="https://www.freecodecamp.org/certification/Mackenzie-May/front-end-development-libraries">
-                <img src={frontEndCertificate} alt="frontendCert" width="100%" display="block"/>
-                </a>
-            </div>
+            </a>
+            <a class="certificate" href="https://www.freecodecamp.org/certification/Mackenzie-May/front-end-development-libraries">
+                <p class="certificateHeader">Front end technologies</p>
+                <img src={frontEndCertificate} alt="frontendCert" width="100%" display="block"/> 
+            </a>
         </div>
     </div>
   );
